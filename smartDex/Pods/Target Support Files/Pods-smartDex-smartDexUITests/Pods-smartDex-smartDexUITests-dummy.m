c0d4834cf8658a80310f1d790d44529173639be5
Nicolás Aponte <91702370+Nicolasaponteb@users.noreply.github.com>
2022-10-19 16:47:29 -0500

    Install Podfile

---
+++ smartDex/Pods/Target Support Files/Pods-smartDex-smartDexUITests/Pods-smartDex-smartDexUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smartDex_smartDexUITests : NSObject
+@end
+@implementation PodsDummy_Pods_smartDex_smartDexUITests
+@end
---
+++ smartDex/Pods/Target Support Files/Pods-smartDex/Pods-smartDex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smartDex : NSObject
+@end
+@implementation PodsDummy_Pods_smartDex
+@end
